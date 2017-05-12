e82a3c3c5ce5eb46203104f0b86186432d083ffc
Muqsit Rayyan <Muqsit@users.noreply.github.com>
2017-05-12 19:55:17 +0300

    Fix final stage of cancer

--- src/Muqsit/FakeBlood.php
+++ src/Muqsit/FakeBlood.php
@@ -1,28 +1,27 @@
 <?php
 namespace Muqsit;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\event\entity\{EntityDamageEvent, EntityDamageByEntityEvent};
-use pocketmine\player;
+use pocketmine\event\Listener;
 use pocketmine\level\particle\RedstoneParticle;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
 
 class FakeBlood extends PluginBase implements Listener{
 
-  public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-  }
-  
-  public function inPvP(EntityDamageEvent $e){
-    if($e->isCancelled()) return;
-    if($e->instanceof EntityDamageByEntityEvent){
-      if($e->getEntity() instanceof Player && $e->getDamager() instanceof Player){
-        switch(mt_rand(1, 5){
-          case 5:
-            $e->getEntity()->getLevel()->addParticle(new RedstoneParticle(new Vector3($e->getEntity()->x, $e->getEntity()->y, $e->getEntity()->z));
-          break;
-        }
-      }
-    }
-  }
+	const CHANCE = 5;
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+
+	public function inPvP(EntityDamageEvent $e){
+		if($e->instanceof EntityDamageByEntityEvent){
+			if(!$e->isCancelled() and (mt_rand(1, self::CHANCE) === 1)){
+				if($e->getEntity() instanceof Player and $e->getDamager() instanceof Player){
+					$e->getEntity()->getLevel()->addParticle(new RedstoneParticle($e->getEntity()));
+				}
+			}
+		}
+	}
 }
